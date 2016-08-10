0151f1cfd173fcf2d0fbf8a186205eebef1d43f6
alexlbr <alex.lobera@gmail.com>
2016-08-10 12:31:16 +0100

    fixd bug in container with injectedProps

--- src/container.js
+++ src/container.js
@@ -3,26 +3,26 @@ import React from 'react';
 class Container extends React.Component {
   constructor(props) {
     super(props);
-    this.setProps(props.injectProps);
+    if (props.injectProps) {
+      this.setProps(props.injectProps);
+    }
     for (let key in props) {
-      const prop = props.injectProps[key] || props[key];
+      let prop = props.injectProps ? props.injectProps[key] || props[key] : props[key];
       if (!this.hasOwnProperty(key) && typeof prop === 'function') {
-        this[key] = prop.bind(this);;
+        this[key] = prop;
       }
     }
   }
-
   setProps(props) {
     this.childNewProps = Object.assign({}, this.childNewProps, props);
   }
 
   bindProp(prop) {
-      this[prop] = this[prop].bind(this);
-      const newProp = {};
-      newProp[prop] = this[prop];
-      this.childNewProps = Object.assign({}, this.childNewProps, newProp);
+    this[prop] = this[prop].bind(this);
+    const newProp = {};
+    newProp[prop] = this[prop];
+    this.childNewProps = Object.assign({}, this.childNewProps, newProp);
   }
-
   render() {
     let ChildComponent = this.props.injectComponent || this.containerComponent;
     return (
