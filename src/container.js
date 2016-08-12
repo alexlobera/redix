5a9332aced73c573c84b8f276d27cb8be6b3c5f9
alexlbr <alex.lobera@gmail.com>
2016-08-12 23:05:51 +0100

    renamed parameters in container.js

--- src/container.js
+++ src/container.js
@@ -3,30 +3,35 @@ import React from 'react';
 class Container extends React.Component {
   constructor(props) {
     super(props);
-    if (props.injectProps) {
-      this.setProps(props.injectProps);
+    if (props.props) {
+      this.setProps(props.props);
     }
     for (let key in props) {
-      let prop = props.injectProps ? props.injectProps[key] || props[key] : props[key];
+      let prop = props.props ? props.props[key] || props[key] : props[key];
       if (!this.hasOwnProperty(key) && typeof prop === 'function') {
         this[key] = prop;
       }
     }
   }
+  
+  setComponent(component) {
+    this.childComponent = component;
+  }
+
   setProps(props) {
-    this.childNewProps = Object.assign({}, this.childNewProps, props);
+    this.childComponentProps = Object.assign({}, this.childComponentProps, props);
   }
 
   bindProp(prop) {
     this[prop] = this[prop].bind(this);
     const newProp = {};
     newProp[prop] = this[prop];
-    this.childNewProps = Object.assign({}, this.childNewProps, newProp);
+    this.childComponentProps = Object.assign({}, this.childComponentProps, newProp);
   }
   render() {
-    let ChildComponent = this.props.injectComponent || this.containerComponent;
+    let ChildComponent = this.props.injectComponent || this.childComponent;
     return (
-      <ChildComponent {...this.props} {...this.childNewProps}/>
+      <ChildComponent {...this.childComponentProps}/>
     );
   }
 }
